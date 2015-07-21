d55db54df7a0afea67f47bf2d10b04c11da0452c
Anthony Mugendi <ngurumugz@gmail.com>
2015-07-21 09:24:26 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+var S = require('string');
+
+module.exports=function(string,stopwords){
+
+	var words=[];
+
+	S(string).strip(stopwords) //remove certain noise words within numbers
+			 .collapseWhitespace() //remove puncts
+			 .trim()							 
+			 .split(' ')
+			 .forEach(function(word){
+
+			 	
+			 	word=word
+			 			.replace(/[\.\?!]$/,'')//remove some ending puncts
+			 			.replace(/([a-z])([A-Z])/g, '$1' + ' ' + '$2') //decamelize
+			 			.replace(/([^a-zA-Z0-9])([a-zA-Z])/g, '$1' + ' ' + '$2') //decamelize on non letters
+			 			.replace(/([a-zA-Z])([^a-zA-Z0-9])/g, '$1' + ' ' + '$2') //decamelize on non letters
+			 			.split(' ') //add all the words
+			 			.forEach(function(w){
+			 				if(w.length){words.push(w);}				 				
+			 			});
+
+			 });
+
+	// console.log(words)
+	return words;
+
+}
---
+++ test.js
@@ -0,0 +1,6 @@
+
+var split = require('./index');
+
+var words = split('split these words for me please! andDeecamelizeThese oh&these_Too');
+
+console.log(words)
